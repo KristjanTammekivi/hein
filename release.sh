5eaf0b3573e16f6462573bd36fca57886bb00e78
Kristjan Tammekivi <kristjantammekivi@gmail.com>
2022-08-21 14:05:12 +0300

    chore: add changelog generation

--- release.sh
+++ release.sh
@@ -21,8 +21,9 @@ if [ $2 != "patch" -a $2 != "minor" -a $2 != "major" ]; then
 fi
 
 yarn --cwd packages/$1 version --$2 --no-git-tag-version
-git add packages/$1/package.json
 VERSION=$(node -p "require('./packages/$1/package.json').version")
+yarn exec -- auto-changelog --tag-prefix $1@ --append-git-log '.packages/$1/' --output packages/$1/CHANGELOG.md -v $VERSION
+git add packages/$1/package.json packages/$1/CHANGELOG.md
 TAG="$1@$VERSION"
 git commit -m "Release $TAG"
 git tag -a $TAG -m "Release $TAG"
