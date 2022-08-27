b809f807d03e462012a8cf226075f9ae09f67f92
Kristjan Tammekivi <kristjantammekivi@gmail.com>
2022-08-27 23:55:32 +0300

    chore: fix release.sh changelog generation

--- release.sh
+++ release.sh
@@ -22,10 +22,18 @@ fi
 
 yarn --cwd packages/$1 version --$2 --no-git-tag-version
 VERSION=$(node -p "require('./packages/$1/package.json').version")
-yarn exec -- auto-changelog --tag-prefix $1@ --append-git-log '.packages/$1/' --output packages/$1/CHANGELOG.md -v $VERSION
-git add packages/$1/package.json packages/$1/CHANGELOG.md
 TAG="$1@$VERSION"
-git commit -m "Release $TAG"
+git add packages/$1/package.json
+git commit -m "chore: Release $TAG"
 git tag -a $TAG -m "Release $TAG"
+yarn exec -- auto-changelog \
+  --tag-prefix $1@ \
+  --append-git-log "packages/$1/" \
+  --output packages/$1/CHANGELOG.md \
+  --ignore-commit-pattern '(^chore|dependabot)' \
+  --commit-limit false
+git add packages/$1/CHANGELOG.md
+git commit --amend --no-edit
+git tag -a $TAG -f -m "Release $TAG"
 git push
 git push origin $TAG
